ccfeaf7a1201f6f07b3c50023b1a946a342bc0d7
hoyennhi489 <hyn489@gmail.com>
2026-04-14 17:47:25 +0700

    Part 1 - Exercise 6: print greeting using variable name

---
+++ Part 1/part01-Part01_06.HiAdaLovelace/HiAdaLovelace.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class HiAdaLovelace {
+
+    public static void main(String[] args) {
+        String name = "Ada Lovelace";
+
+        System.out.println("Hi " + name + "!");
+    }
+}
