d3cbcd4a5fb25a29ee790aa31dbc0db0d033f19a
hoyennhi489 <hyn489@gmail.com>
2026-04-20 20:37:30 +0700

    Part 2: complete exercises 12 to 16 (loops and number processing)

---
+++ Part 2/part02-Part02_12.AverageOfNumbers/AverageOfNumbers.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class AverageOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        int count = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int num = Integer.valueOf(scanner.nextLine());
+
+            if (num == 0) {
+                break;
+            }
+
+            sum += num;
+            count++;
+        }
+
+        double average = (double) sum / count;
+
+        System.out.println("Average of the numbers: " + average);
+    }
+}
